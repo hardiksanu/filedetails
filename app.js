205de3a9f5c61fe7ac6fd70d3f7801a17bb12068
hardikpatel <hardiksanu90@gmail.com>
2022-12-22 18:07:29 +0530

    last commit

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const path = require('path');
 // const download = require('download');
 const fs = require('fs');
 const ls = require('ls');
-var prettysize = require('prettysize');
+const prettysize = require('prettysize');
 const directory = path.join('C:/Users/Hardik/Desktop/Text_file/');
 // const savepath = path.join('./download');
-var cors = require('cors');
+const cors = require('cors');
 
 // to use json data in express app. through use of postman and get in this app, we needs to get permission from express app.
 app.use(express.json());
@@ -71,26 +71,90 @@ app.get('/download/:filename', (req, res) => {
                     res.status(400).send(e);
           }
 });
-
+/*
 app.get('/event' , (req, res) => {
           try{
-                    let count = 0;
+                    // let count = 0;
                     res.writeHead(200, {
                               'Cache-Control': 'no-cache',
                               'Connection': "keep-alive",
                               'Content-Type': 'text/event-stream', 
                     });
                     setInterval(() => {
-                              count += 1;
-                              obj = {count};
-                              console.log(count);
-                              res.status(200).write(`data: ${JSON.stringify(obj)}\n\n`);
-                              // res.write(` ${JSON.stringify(obj)}\n\n`);
+                              // count += 1;
+                              // obj = {count};
+                              // console.log(count);
+                              const data = new Date().toLocaleString();
+                              console.log(data);
+                              // res.status(200).write(`data: ${JSON.stringify(obj)}\n\n`);
+                              res.status(200).write(`data: ${data}\n\n`);
                     }, 5000);
           }
           catch (e) {
                     res.status(400).send(e);
           }
 })
+*/
+app.get('/event' , async (req, res) => {
+          try{
+                    let count = 0;
+                    console.log("Client Connected");
+                    res.writeHead(200, {
+                              'Cache-Control': 'no-cache',
+                              'Connection': "keep-alive",
+                              'Content-Type': 'text/event-stream', 
+                    });
+                    const intervalID = await setInterval(() => {
+                              count += 1;
+                              obj = {count};
+                              console.log(count);
+                              // const data = new Date().toLocaleString();
+                              // console.log(data);
+                              res.status(200).write(`data: ${JSON.stringify(obj)}\n\n`);
+                              // res.status(200).write(`data: ${data}\n\n`);
+                    }, 10000);
 
+                    res.on('close', () => {
+                              console.log("Client closed connection");
+                              clearInterval(intervalID);
+                    });
+          }
+          catch (e) {
+                    res.status(400).send(e);
+          };
+});
+
+app.get('/start', (req, res) => {
+          try {
+                    console.log(req.body);
+                      res.status(200).send("Application started successfully.");
+          }
+          catch (e) {
+                    res.status(400).send(e);
+
+          }
+
+});
+app.get('/stop', (req, res) => {
+          try {
+                    console.log(req.body);
+                    res.status(200).send("Application stopped successfully.");
+          }
+          catch (e) {
+                    res.status(400).send(e);
+
+          }
+
+});
+
+app.post('/create', (req, res) => {
+          try {
+                    console.log(req.body);
+                    res.status(200).send(req.body);
+          }
+          catch (e) {
+                    res.status(400).send(e);
+
+          }
+});
 module.exports = app;
--- index.js
+++ index.js
@@ -3,12 +3,10 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-
-const {API_PORT} = process.env;
-
-const port = process.env.PORT || API_PORT;
+const API_PORT = 9090;
+// const port = process.env.PORT || API_PORT;
 
 //SERVER LISTENING
-server.listen(port, ()=> {
+server.listen(API_PORT, ()=> {
           console.log(`server running on port ${API_PORT}`);
 });
